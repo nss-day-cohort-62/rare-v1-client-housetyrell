fe8767c4d06cc8f31551b270189f42224714f686
Jordan Victoria <jgomeskuehner@gmail.com>
2023-04-19 10:22:55 -0500

    filter trouble

--- src/components/posts/postForm.js
+++ src/components/posts/postForm.js
@@ -61,7 +61,8 @@ export const PostForm = () => {
         title: post.title,
         publication_date: post.publication_date,
         image_url: post.image_url,
-        content: post.content
+        content: post.content,
+        approved: 1
       })
        .then(() => navigate(`/postDetails/${postId}`))
     }
